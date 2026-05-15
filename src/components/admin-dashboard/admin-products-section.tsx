dd4ffbf0d3fa491c4252c5620517ad26f0317a7e
Mohamed Khaled Ahmed <192300448@ecu.edu.eg>
2026-05-15 16:18:02 +0300

    lol

--- src/components/admin-dashboard/admin-products-section.tsx
+++ src/components/admin-dashboard/admin-products-section.tsx
@@ -78,6 +78,7 @@ export function AdminProductsSection({
   onQueryChange,
   onSortChange,
   onSortDirectionChange,
+  onStockFilterChange,
   onCreateProduct,
   onNewProductChange,
   onNewImageSelect,
